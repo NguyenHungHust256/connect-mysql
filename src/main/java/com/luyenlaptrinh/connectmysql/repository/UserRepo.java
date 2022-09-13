a30d799d63b341a973e8a27ae93aa1fee2e8027b
xnk_huydq1 <huydq1@viettelimex.vn>
2022-09-13 20:40:34 +0630

    update repository

--- src/main/java/com/luyenlaptrinh/connectmysql/repository/UserRepo.java
+++ src/main/java/com/luyenlaptrinh/connectmysql/repository/UserRepo.java
@@ -2,7 +2,21 @@ package com.luyenlaptrinh.connectmysql.repository;
 
 import com.luyenlaptrinh.connectmysql.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User, String> {
+    List<User> findByName(String name);
 
+    List<User> findByNameAndGender(String name, String gender);
+
+    Optional<User> findById(String id);
+
+    @Query(value = "SELECT * FROM USER WHERE NAME = ?1", nativeQuery = true)
+    List<User> getUserByName(String name);
+
+    @Query(value = "SELECT * FROM USER WHERE NAME = ?1 AND GENDER = ?2", nativeQuery = true)
+    List<User> getUserByNameAndGender(String name, String gender);
 }
--- src/main/java/com/luyenlaptrinh/connectmysql/service/UserService.java
+++ src/main/java/com/luyenlaptrinh/connectmysql/service/UserService.java
@@ -2,6 +2,9 @@ package com.luyenlaptrinh.connectmysql.service;
 
 import com.luyenlaptrinh.connectmysql.entities.User;
 
+import java.util.List;
+
 public interface UserService {
     User getUserById(String id);
+    List<User> getAllUsers();
 }
